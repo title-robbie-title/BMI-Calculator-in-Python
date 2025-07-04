bd23bfebfc4a49c2bf19d6972d8229463c6b2065
Robbie <duncanrobbie1998@gmail.com>
2025-07-04 15:42:37 +0100

    Update BMI_Calculator.py
    I have added a function to tell the user whether their BMI is underweight, normal weight, overweight or obese.

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,4 +1,11 @@
 weight = float(input("Enter your weight in kilograms: "))
 height = float(input("Enter your height in meters: "))
 BMI = weight/(height**2)
-print (f"your BMI is: {BMI: .2f}")
+if BMI < 18.5:
+    print (f"your BMI is:{BMI: .2f} meaning you are underweight.")
+elif 18.5 <= BMI < 25 :
+    print (f"your BMI is:{BMI: .2f} meaning you are normal weight.")
+elif 25 <= BMI < 30: 
+     print (f"your BMI is:{BMI: .2f} meaning you are overweight.")
+else:
+    print (f"your BMI is:{BMI: .2f} meaning you are obese.")
